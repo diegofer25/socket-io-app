3ae6ef08796bfc292ea71b5b84e80cfdf9f98c48
Diego Lamarão <diego.lamarao92@gmail.com>
2020-11-07 17:22:56 -0300

    add user route

--- index.ts
+++ index.ts
@@ -7,6 +7,12 @@ app.use(express.urlencoded());
 
 app.get("/", (req, res) => res.send("Hello from express"));
 
+app.get("/user/:name", (req, res) => {
+  const { name } = req.params;
+
+  res.send(`The user name is ${name}`);
+});
+
 app.listen(PORT, () => {
   console.log("Server is running at port", PORT);
 });
